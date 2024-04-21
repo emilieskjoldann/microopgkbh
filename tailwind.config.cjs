a483673c09b10377941c727c17994373b42e5938
Emilie Skjoldann <emilie_skjoldann@outlook.dk>
2024-04-21 21:13:16 +0200

    k

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,4 +7,16 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
     ],
+    theme: {
+        screens: {
+          'tablet': '640px',
+          // => @media (min-width: 640px) { ... }
+    
+          'laptop': '1024px',
+          // => @media (min-width: 1024px) { ... }
+    
+          'desktop': '1280px',
+          // => @media (min-width: 1280px) { ... }
+        },
+      }
   };
